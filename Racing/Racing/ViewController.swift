e677395cc3015ecb3a1fa295c0b9e73c3571ff33
Pavel Akulenok <paulakulenok@gmail.com>
2021-03-22 16:58:15 +0300

    add project and start screen

---
+++ Racing/Racing/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Racing
+//
+//  Created by Pavel Akulenak on 22.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
