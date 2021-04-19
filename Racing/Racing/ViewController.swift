5440f480e6e45d8fe7ff43bda32f148031b1e2ae
Pavel Akulenok <paulakulenok@gmail.com>
2021-04-19 13:22:58 +0300

    redesigned start screen UI

--- Racing/Racing/ViewController.swift
+++ Racing/Racing/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var startScreenBackgroundImageView: UIImageView!
+    @IBOutlet weak var enterGameButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startScreenBackgroundImageView.image = UIImage(named: "startScreenBackground")
+        enterGameButton.layer.cornerRadius = 20
     }
-    
-}
 
+    @IBAction private func onEnterGameButton(_ sender: UIButton) {
+    }
+}
